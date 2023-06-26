a0b8a45cf17e17cf1241763cbffbe3b508081ae4
LAPTOP-UQDCE535 <nana.botchway@amalitech.org>
2023-06-26 03:18:42 +0000

    React Build

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import { BrowserRouter as Router, Route, Link } from "react-router-dom"
+import Header from "./directories/Header"
+import Search from "./directories/Search"
+import Countries from "./directories/Countries"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Header />
+      <Search />
+      <Countries />
+    </>
+  )
 }
 
-export default App;
+export default App
